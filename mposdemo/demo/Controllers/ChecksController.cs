6be83c4bd78c2b85f8ff0e7bf6cfb583df9a9c6c
Rohith Vallabhaneni <rohithvallabhaneni@Rohiths-MacBook-Pro.local>
2018-03-14 13:04:39 +0100

    Sample dotnet core application

---
+++ mposdemo/demo/Controllers/ChecksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo.Controllers
+{
+    [Route(".well-known")]
+    public class ChecksController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("live")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("ready")]
+        public async Task<IActionResult> GetReady()
+        {
+            return Ok("readdy");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
